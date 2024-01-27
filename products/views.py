9e2fe6059320e494af26162a59b0f169a7843d17
Stacey Lewis <stacey.lws@googlemail.com>
2024-01-27 19:38:05 +0000

    Feat: Add products to products page including images

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def all_products(request):
+    """ Displays all products, including sorting and searching """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
