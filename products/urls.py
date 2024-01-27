31e41f890511310045916902867ab16ede8b4a18
Stacey Lewis <stacey.lws@googlemail.com>
2024-01-27 22:46:15 +0000

    Feat: Add product description page

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_products, name='all_products')
+    path('', views.all_products, name='all_products'),
+    path('<product_id>', views.product_description, name='product_description'),
 ]
