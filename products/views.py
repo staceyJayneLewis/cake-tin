0ef97417674a2bcbf5ca6e2fc0f02d12aeff052c
Stacey Lewis <stacey.lws@googlemail.com>
2024-01-30 13:04:47 +0000

    Feat: Add search functionality to form input

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect,reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 # Create your views here.
@@ -7,9 +9,22 @@ def all_products(request):
     """ Displays all products, including sorting and searching """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter a product to search, please enter the product you are searching for.")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_item':query,
     }
 
     return render(request, 'products/products.html', context)
