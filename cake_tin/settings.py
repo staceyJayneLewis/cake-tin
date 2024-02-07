89f27aa931b20a16fff17f3ccb900737eb9749a4
Stacey Lewis <stacey.lws@googlemail.com>
2024-02-07 23:08:54 +0000

    Fix: AWS key name error

--- cake_tin/settings.py
+++ cake_tin/settings.py
@@ -167,8 +167,8 @@ if 'USE_AWS' in os.environ:
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'cake-tin'
     AWS_S3_REGION_NAME = 'us-east-1'
-    AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
-    AWS_SECRET_KEY = os.environ.get('AWS_SECRET_KEY')
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
