53b09b78d755dd97af91f10c687bc3a46cc662a9
Stacey Lewis <stacey.lws@googlemail.com>
2024-02-14 12:46:45 +0000

    Feat: Add category page header depending on category selected

--- products/views.py
+++ products/views.py
@@ -16,7 +16,7 @@ def all_products(request):
         if 'category' in request.GET:
                 categories = request.GET['category'].split(',')
                 products = products.filter(category__name__in=categories)
-                categories = Category.objects.filter(name__in=categories)
+                categories = Category.objects.filter(name__in=categories).first()
                     
         if 'q' in request.GET:
             search_request = request.GET['q']
