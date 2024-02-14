5030e34be31b50a220441e0f8b307312f7a2c99c
Stacey Lewis <stacey.lws@googlemail.com>
2024-02-14 11:32:00 +0000

    Update product models to remove unused fields

--- products/admin.py
+++ products/admin.py
@@ -4,7 +4,6 @@ from .models import Product, Category
 # Register your models here.
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
-        'sku',
         'name',
         'category',
         'price',
