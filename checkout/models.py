24eb1ec57300e9dfd59d0bf4b73b67a4cccabb6f
Stacey Lewis <stacey.lws@googlemail.com>
2024-02-23 19:33:40 +0000

    Fix: Add meta name on models and change order of information

--- checkout/models.py
+++ checkout/models.py
@@ -5,17 +5,20 @@ from products.models import Product
 import uuid
 
 class Order_details(models.Model):
+    class Meta: 
+        verbose_name_plural = 'Orders'
+
     order_number = models.CharField(max_length=30, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
-    contact_number = models.CharField(max_length=20, null=False, blank=False)
     address_line_1 = models.CharField(max_length=80, null=False, blank=False)
     address_line_2 = models.CharField(max_length=80, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = models.CharField(max_length=40, null=False, blank=False)
-    delivery = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    contact_number = models.CharField(max_length=20, null=False, blank=False)
     sub_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     date = models.DateTimeField(auto_now_add=True)
 
@@ -24,7 +27,7 @@ class Order_details(models.Model):
 
         return uuid.uuid4().hex.upper()
 
-    def grand_total_update(self):
+    def order_total_update(self):
         """Update grand total each time new item is added including delivery"""
 
         self.sub_total = self.items_ordered.aggregate(Sum('item_ordered_total'))['order_total__sum']
@@ -42,7 +45,10 @@ class Order_details(models.Model):
         return self.order_number
 
 
-class Item_ordered(models.Model):
+class Item_ordered(models.Model):   
+    class Meta: 
+        verbose_name_plural = 'Orders'
+
     order = models.ForeignKey(Order_details, null=False, blank=False, on_delete=models.CASCADE, related_name='items_ordered')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     quantity = models.IntegerField(null=False, blank=False, default=0)
