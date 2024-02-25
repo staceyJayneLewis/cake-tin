55a8ebd29df4d9dae28b035ddc1fd01ff5ee2382
Stacey Lewis <stacey.lws@googlemail.com>
2024-02-25 14:31:36 +0000

    Docs: Add checkout templates, url and views

--- cake_tin/urls.py
+++ cake_tin/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('basket/', include('basket.urls')),
     path('contact/', include('contact.urls')),
+    path('checkout/', include('checkout.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
+from django.contrib import messages
+from .forms import form_order_request
 
-# Create your views here.
+def checkout(request):
+    basket_session = request.session.get('basket_session', {})
+    if not basket_session:
+        messages.error(request, 'There are no items in your basket')
+        return redirect(reverse('all_products'))
+
+
+    order_request = form_order_request()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_request': order_request,
+    }
+
+    return render(request, template, context)
