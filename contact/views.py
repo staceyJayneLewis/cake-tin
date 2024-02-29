cb8756091f1c8501301fa718b497aa34238d2325
Stacey Lewis <stacey.lws@googlemail.com>
2024-02-29 15:27:30 +0000

    Model: Add contact form model instead of hardcoded form

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
+
 
 def contact(request):
     """Return to the contact page """
-    return render(request, 'contact/contact-us.html')
+    form = ContactForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request,
+                "Thanks for your message we will be in touch soon! "
+            )
+            return redirect(contact)
+        messages.error(
+                request,
+                "Error, please try again!"
+            )
+    template = 'contact/contact-us.html'
+    context = {
+        "form": form,
+    }
+    return render(request, template, context)
