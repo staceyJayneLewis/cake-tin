0302c0f9856139074a01f79481185fd047350342
Stacey Lewis <stacey.lws@googlemail.com>
2024-03-05 15:04:50 +0000

    Fix: countryfield issue on checkout model

--- checkout/models.py
+++ checkout/models.py
@@ -15,7 +15,7 @@ class Order_details(models.Model):
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     contact_number = models.CharField(max_length=20, null=False, blank=False)
-    country = CountryField(blank_label='Country *',null=True, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     address_line_1 = models.CharField(max_length=80, null=False, blank=False)
