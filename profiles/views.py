c30b93ce0932b83d63170a488079c714ab0ae2b9
Stacey Lewis <stacey.lws@googlemail.com>
2024-03-06 00:26:04 +0000

    Feat: Add and style user profile and link to base template

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import UserProfile
 
 def profile(request):
     "return profile page of user"
 
-    template = "profiles/profile.html"
-    context = {}
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
+    
