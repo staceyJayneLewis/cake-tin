bafbead0e2c8f1e83a0a6c3e65da3bda12eea045
Stacey Lewis <stacey.lws@googlemail.com>
2024-03-06 22:14:11 +0000

    Feat: Finish CRUD functionality with edit and delete options

--- products/views.py
+++ products/views.py
@@ -56,7 +56,7 @@ def add_product(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_description'))
         else:
             messages.error(request, 'Unable to add product. Please ensure the form is valid.')
     else:
@@ -68,3 +68,36 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """ Edit a product in the store from the admin """
+
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductFormAdmin(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_description', args=[product.id]))
+        else:
+            messages.error(request, 'Could not update product. Please ensure the form is filled in correctly.')
+    else:
+        form = ProductFormAdmin(instance=product)
+        messages.success(request, f'Edit {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def delete_product(request, product_id):
+    """ Delete a product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product successfully deleted!')
+    return redirect(reverse('all_products'))
