66fa544329e49d6c2052f1ec5b440a40866d2187
Stacey Lewis <stacey.lws@googlemail.com>
2024-03-07 16:17:17 +0000

    Fix: update webhook variable name

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -12,7 +12,7 @@ import stripe
 def webhook(request):
     """Listen for webhooks from Stripe"""
     # Setup
-    wh_secret = settings.STRIPE_WEBHOOK_SECRET_KEY
+    wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
     # Get data from webhook and check signature
