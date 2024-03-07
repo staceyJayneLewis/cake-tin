d78b0ccfb71189dfa22f4ba96bcdf86f477ab762
Stacey Lewis <stacey.lws@googlemail.com>
2024-03-07 16:17:48 +0000

    Feat: add random cupcake image to home carousel

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
 
 def index(request):
     """Return to the index page """
-    return render(request, 'home/index.html')
+    products = Product.objects.order_by('?')[:2]
+    template = 'home/index.html'
+    context = {
+        'products': products,
+    }
+    return render(request, template, context)
